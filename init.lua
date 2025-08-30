ac476d81c933b9e5fe40a98b4fa2d7f4423583a6
Roman Ma <rma@precog.com>
2025-08-30 12:21:57 +0100

    add codeium; change shell priorities

--- init.lua
+++ init.lua
@@ -1,9 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-if vim.fn.has("macunix") then
+if vim.fn.executable("xonsh") == 1 then
   vim.opt.shell = "xonsh"
-elseif vim.fn.has("win64") then
+elseif vim.fn.executable("fish") == 1 then
+  vim.opt.shell = "fish"
+elseif vim.fn.executable("zsh") == 1 then
+  vim.opt.shell = "zsh"
+elseif vim.fn.executable("powershell") == 1 then
   vim.opt.shell = "powershell.exe"
 end
 
