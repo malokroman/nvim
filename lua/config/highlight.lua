fb6de6a3cd08732bdcf8bfb09374d50c138ef244
Roman Ma <roman.ma@Romans-Mac-mini.local>
2025-09-25 13:41:21 +0100

    Markdown latex highlight

--- lua/config/highlight.lua
+++ lua/config/highlight.lua
@@ -1,11 +1,4 @@
 -- Theme
-local nonbinary_hl = {
-  "Yellow",
-  "White",
-  "Purple",
-  "Grey",
-}
-
 vim.api.nvim_set_hl(0, "TreesitterContext", { bg = nil, bold = true, underdotted = true })
 
 vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = nil, italic = true })
@@ -18,3 +11,5 @@ vim.api.nvim_set_hl(0, "RenderMarkdownH3Bg", { bg = "#cfcc53", fg = "#FFFFFF" })
 vim.api.nvim_set_hl(0, "RenderMarkdownH4Bg", { bg = "#cc53cf", fg = "#EEEEEE" })
 vim.api.nvim_set_hl(0, "RenderMarkdownH5Bg", { bg = "#53bcdf", fg = "#FFFFFF" })
 vim.api.nvim_set_hl(0, "RenderMarkdownH6Bg", { bg = "#cfcc53", fg = "#FFFFFF" })
+
+vim.api.nvim_set_hl(0, "RenderMArkdownMath", { bold = true })
